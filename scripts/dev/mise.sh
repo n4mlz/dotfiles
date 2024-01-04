50ab1bac26be00ffe690621fbc21a62f666b6b7f
Nameless-itf23 <ohutoncat@gmail.com>
2024-01-04 19:12:57 +0900

    mise.sh

--- scripts/dev/mise.sh
+++ scripts/dev/mise.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-main() {
+apt_brew() {
     sudo install -dm 755 /etc/apt/keyrings
     wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
     echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
     sudo apt update
     sudo apt install -y mise
+}
+
+pacman_yay() {
+    # install scripts
+    echo "not supported yet"
+}
+
+main() {
+    if command -v apt &>/dev/null; then
+        echo "using apt and brew..."
+        apt_brew
+    elif command -v pacman &>/dev/null; then
+        echo "using pacman and yay..."
+        pacman_yay
+    else
+        echo "pm not found"
+    fi
 }
 
 main
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,43 +3,43 @@
 cd $(dirname $0)
 
 apt_brew() {
-  sudo apt install -y curl wget git zsh vim build-essential ubuntu-advantage-tools
-  if ! command -v brew &>/dev/null; then
-    if [ ! -d /home/linuxbrew ]; then
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    sudo apt install -y curl wget git zsh vim build-essential ubuntu-advantage-tools
+    if ! command -v brew &>/dev/null; then
+        if [ ! -d /home/linuxbrew ]; then
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        sudo chmod -R 777 /home/linuxbrew
     fi
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    sudo chmod -R 777 /home/linuxbrew
-  fi
-  brew install sheldon
-  brew install starship
+    brew install sheldon
+    brew install starship
 }
 
 pacman_yay() {
-  # install scripts
-  echo "not supported yet"
+    # install scripts
+    echo "not supported yet"
 }
 
 install() {
-  if command -v apt &>/dev/null; then
-    echo "using apt and brew..."
-    apt_brew
-  elif command -v pacman &>/dev/null; then
-    echo "using pacman and yay..."
-    pacman_yay
-  else
-    echo "pm not found"
-  fi
+    if command -v apt &>/dev/null; then
+        echo "using apt and brew..."
+        apt_brew
+    elif command -v pacman &>/dev/null; then
+        echo "using pacman and yay..."
+        pacman_yay
+    else
+        echo "pm not found"
+    fi
 }
 
 deploy() {
-  ./deploy.sh
-  sudo chsh -s /bin/zsh
+    ./deploy.sh
+    sudo chsh -s /bin/zsh
 }
 
 main() {
-  install
-  deploy
+    install
+    deploy
 }
 
 main
