6432bfa84aceb1d4ad0a0cc89a46286209512b03
Nameless-itf23 <ohutoncat@gmail.com>
2024-02-09 16:03:32 +0900

    feat: fundamental tools

---
+++ scripts/dev/fundamentals.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt_brew() {
+    sudo apt install -y git binutils make gcc fakeroot wget curl which acpi vi vim less man neofetch strace ltrace build-essential ubuntu-advantage-tools
+}
+
+pacman_yay() {
+    sudo pacman -S git binutils make gcc fakeroot wget curl which acpi vi vim less man neofetch strace ltrace --noconfirm
+}
+
+main() {
+    if command -v apt &>/dev/null; then
+        echo "using apt and brew..."
+        apt_brew
+    elif command -v pacman &>/dev/null; then
+        echo "using pacman and yay..."
+        pacman_yay
+    else
+        echo "pm not found"
+    fi
+}
+
+main
