0bc293271f191ccfa0b05c13c42e2053eafa20da
Nameless-itf23 <ohutoncat@gmail.com>
2024-02-09 23:12:51 +0900

    feat: yay

---
+++ scripts/dev/yay.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt_brew() {
+    echo "apt: not supported yet"
+}
+
+pacman_yay() {
+    sudo pacman -S openssh
+    setting
+}
+
+setting() {
+    sudo systemctl enable sshd
+    sudo systemctl start sshd
+}
+
+main() {
+    if command -v apt &>/dev/null; then
+        echo "using apt and brew..."
+        apt_brew
+    elif command -v pacman &>/dev/null; then
+        echo "using pacman and yay..."
+        pacman_yay
+    else
+        echo "pm not found"
+    fi
+}
+
+main
