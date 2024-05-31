1c459edc61bc3b97c8429c295afc14569a2e6aa2
n4mlz <ohutoncat@gmail.com>
2024-05-31 15:09:22 +0900

    feat: add lsp on astronvim

--- files/.config/nvim/lua/plugins/astrolsp.lua
+++ files/.config/nvim/lua/plugins/astrolsp.lua
@@ -45,7 +45,7 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
