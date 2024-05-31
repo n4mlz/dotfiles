a9b1daba93d694f0448fc11c7073dab767e0acf1
n4mlz <ohutoncat@gmail.com>
2024-05-31 15:21:34 +0900

    change: config on astronvim

--- files/.config/nvim/lua/plugins/user.lua
+++ files/.config/nvim/lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -81,5 +79,28 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+          },
+          never_show = {
+            ".git",
+          },
+        },
+      },
+    },
   },
 }
