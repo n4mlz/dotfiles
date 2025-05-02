c811e97407bd5c562ce7f2a9c1a62cc8d7d95dbe
n4mlz <n4mlz.dev@gmail.com>
2025-05-02 16:18:54 +0900

    :truck: move old nvim config and install astronvim v5

--- files/.config/nvim-old/lua/plugins/astrolsp.lua
+++ files/.config/nvim-old/lua/plugins/astrolsp.lua
@@ -12,6 +12,7 @@ return {
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
+      autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
       inlay_hints = false, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
@@ -44,7 +45,7 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
@@ -90,7 +91,7 @@ return {
           function() require("astrolsp.toggles").buffer_semantic_tokens() end,
           desc = "Toggle LSP semantic highlight (buffer)",
           cond = function(client)
-            return client.supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
+            return client.supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens
           end,
         },
       },
--- files/.config/nvim-old/lua/plugins/user.lua
+++ files/.config/nvim-old/lua/plugins/user.lua
@@ -1,7 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
 
 ---@type LazySpec
@@ -18,28 +15,23 @@ return {
 
   -- == Examples of Overriding Plugins ==
 
-  -- customize dashboard options
+  -- customize alpha options
   {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-          }, "\n"),
-        },
-      },
-    },
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        [[                                                      ]],
+        [[██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗]],
+        [[██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝]],
+        [[██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  ]],
+        [[╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  ]],
+        [[ ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗]],
+        [[  ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝]],
+        [[                                                      ]],
+      }
+      return opts
+    end,
   },
 
   -- You can disable default plugins as follows:
@@ -85,4 +77,31 @@ return {
       )
     end,
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+          },
+          never_show = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "wakatime/vim-wakatime",
+  }
 }
