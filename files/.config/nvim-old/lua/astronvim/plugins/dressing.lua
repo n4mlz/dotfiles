4e1d146e3b08cca2687e3084cb9ca34d57cad94a
n4mlz <n4mlz.dev@gmail.com>
2025-05-02 17:11:11 +0900

    :wastebasket: delete old config

--- files/.config/nvim-old/lua/astronvim/plugins/dressing.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/dressing.nvim",
-  lazy = true,
-  init = function() require("astrocore").load_plugin_with_func("dressing.nvim", vim.ui, { "input", "select" }) end,
-  opts = {
-    input = { default_prompt = "➤ " },
-    select = { backend = { "telescope", "builtin" } },
-  },
-}
--- files/.config/nvim-old/lua/astronvim/plugins/ts-autotag.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "windwp/nvim-ts-autotag",
-  event = "User AstroFile",
-  opts = {},
-  config = function(...) require "astronvim.plugins.configs.ts-autotag"(...) end,
-}
